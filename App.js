3efe941d66bae2b6f137f032ae1e7427d0d08100
milton munoz <livemilton@hotmail.com>
2025-05-04 22:42:47 -0500

    install fontAwesome icons - creating components

--- App.js
+++ App.js
@@ -2,10 +2,13 @@
 import React from 'react';
 
 import {
-  SafeAreaView
+  SafeAreaView, TouchableOpacity, View, Text
 } from 'react-native';
 
 import Title from './components/Title/Title';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import globalStyle from './assets/fonts/styles/globalStyle';
 
 
 const App =()=> {
@@ -14,7 +17,17 @@ const App =()=> {
  
   return (
     <SafeAreaView>
+      <View style={
+          globalStyle.header
+        }>
       <Title title={'Let´s Explore '}/>
+      <TouchableOpacity style={globalStyle.messageIcon}>
+        <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'}/>
+        <View style={globalStyle.messageNumberContainer}>
+          <Text style={globalStyle.messageNumber}>2</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
