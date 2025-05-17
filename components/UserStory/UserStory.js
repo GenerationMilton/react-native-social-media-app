c2ae72e97818c26fe6f29dbd8634a98756c46c7a
milton munoz <livemilton@hotmail.com>
2025-05-16 21:07:15 -0500

    Using profile image in flatlist

--- components/UserStory/UserStory.js
+++ components/UserStory/UserStory.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image} from 'react-native';
 import PropTypes from 'prop-types';
-import style from './style';
+import style from '../UserStory/style';
+import UserProfileImage from '../UserProfileImage/UserProfileImage';
 
 const UserStory = (props)=>{
     return (
-        <View style={style.storyContainer}>
             <View style={style.userImageContainer}>
+            <UserProfileImage 
+                    profileImage={props.profileImage}
+                    imageDimensions={65}
+                />
             <Image source={props.profileImage} style={style.image}/>
             </View>
-            <Text style={style.firstName}>{props.firstName}</Text>
-        </View>
     );
 };
 
 
 UserStory.propTypes ={
     firstName: PropTypes.string.isRequired,
-    profileImage: PropTypes.object.isRequired,
 };
 
 export default UserStory;
